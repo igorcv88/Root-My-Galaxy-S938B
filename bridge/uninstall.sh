f480cfe4bbb8c5e97e9017980c49b87839975984
Igor <igorv88@gmail.com>
2026-07-29 20:42:46 -0300

    Clean bridge launcher lock on uninstall

--- bridge/uninstall.sh
+++ bridge/uninstall.sh
@@ -3,3 +3,4 @@ rm -f /data/local/tmp/rmg-rezygisk-arm \
       /data/local/tmp/rmg-rezygisk-bridge.pid \
       /data/local/tmp/rmg-rezygisk-bridge.log \
       /data/local/tmp/rmg-rezygisk-bridge-status
+rm -rf /data/local/tmp/rmg-rezygisk-bridge.lock
