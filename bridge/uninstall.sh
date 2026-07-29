bbc10e40d6b509f3d725b4c83fd4b02ebc3dacc7
Igor <igorv88@gmail.com>
2026-07-29 19:37:21 -0300

    Add bridge cleanup script

---
+++ bridge/uninstall.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+rm -f /data/local/tmp/rmg-rezygisk-arm \
+      /data/local/tmp/rmg-rezygisk-bridge.pid \
+      /data/local/tmp/rmg-rezygisk-bridge.log \
+      /data/local/tmp/rmg-rezygisk-bridge-status
